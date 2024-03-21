4c22ee941b715c1919f4f85e73d5a821062c1f88
Darko <darkovasiljevic740@gmail.com>
2024-03-21 19:50:22 +0800

    Added employee modal component

---
+++ resources/js/Components/EmployeeModal.vue
@@ -0,0 +1,27 @@
+<template>
+	<dialog id="addEmployeeModal" class="modal">
+		<div class="modal-box">
+			<h3 class="text-center text-xl font-bold uppercase">
+				Employee
+			</h3>
+			<div class="form-control">
+				<label class="label" for="fullName">Full Name</label>
+				<input class="input-bordered input" type="text" id="fullName" />
+
+				<label class="label" for="email">Email</label>
+				<input class="input-bordered input" type="text" id="email" />
+
+				<label class="label" for="skills">Skills</label>
+				<textarea class="textarea-bordered textarea" id="skills" placeholder="Web dev, AI, ..."></textarea>
+			</div>
+			<div class="modal-action">
+				<form method="dialog">
+					<button class="btn-primary btn mr-2">
+						Create
+					</button>
+					<button class="btn-neutral btn">Close</button>
+				</form>
+			</div>
+		</div>
+	</dialog>
+</template>
--- resources/js/Pages/Employee.vue
+++ resources/js/Pages/Employee.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import EmployeeTable from "@/Components/EmployeeTable.vue";
+import EmployeeModal from "@/Components/EmployeeModal.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
 
@@ -9,38 +10,16 @@ import { Head } from "@inertiajs/vue3";
 
 	<AuthenticatedLayout>
 		<div class="container py-12">
-			<!-- Create User modal -->
+			<!-- Create User -->
 			<div class="flex justify-end">
-				<button class="btn-info btn mb-4 py-4" onclick="my_modal_1.showModal()">
+				<button class="btn-info btn mb-4 py-4" onclick="addEmployeeModal.showModal()">
 					Add Employee
 				</button>
-				<dialog id="my_modal_1" class="modal">
-					<div class="modal-box">
-						<h3 class="text-center text-xl font-bold uppercase">
-							Employee
-						</h3>
-						<div class="form-control">
-							<label class="label" for="fullName">Full Name</label>
-							<input class="input-bordered input" type="text" id="fullName" />
 
-							<label class="label" for="email">Email</label>
-							<input class="input-bordered input" type="text" id="email" />
-
-							<label class="label" for="skills">Skills</label>
-							<textarea class="textarea-bordered textarea" id="skills" placeholder="Web dev, AI, ..."></textarea>
-						</div>
-						<div class="modal-action">
-							<form method="dialog">
-								<button class="btn-primary btn mr-2">
-									Create
-								</button>
-								<button class="btn-neutral btn">Close</button>
-							</form>
-						</div>
-					</div>
-				</dialog>
+				<EmployeeModal />
 			</div>
 
+			<!-- Employee table -->
 			<div class="bg-white sm:rounded-lg">
 				<EmployeeTable />
 			</div>
