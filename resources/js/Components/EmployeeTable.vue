53c830849e19f5485d877cde7978465726a0a9e3
Darko <darkovasiljevic740@gmail.com>
2024-03-21 17:29:33 +0800

    Created employee component

---
+++ resources/js/Components/EmployeeTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <table class="table">
+        <!-- head -->
+        <thead class="text-center">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Skills</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <tr>
+                <td>Cy Ganderton</td>
+                <td>email@email.com</td>
+                <td>Web development</td>
+                <td>
+                    <button class="btn btn-warning mr-2">Edit</button>
+                    <button class="btn btn-error">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Hart Hagerty</td>
+                <td>email@email.com</td>
+                <td>VA</td>
+                <td>
+                    <button class="btn btn-warning mr-2">Edit</button>
+                    <button class="btn btn-error">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Brice Swyre</td>
+                <td>email@email.com</td>
+                <td>AI</td>
+                <td>
+                    <button class="btn btn-warning mr-2">Edit</button>
+                    <button class="btn btn-error">Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
